28e20d74306b8d311a6d4915ed7f3232dab2f0ec
Koziar <lkoziars@gmail.com>
2015-12-08 13:40:07 +0100

    changed search bar

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -12,82 +12,89 @@ angular.module('myApp.view1', ['ngRoute'])
 
         .controller('View1Ctrl', function ($http, $scope, InfoFactory) {
 
-//            $scope.searchFlights = function(){
-//                
-//                if($scope.flyTo == ''){
-//                    
-//                    getAllFlightsFrom();
-//                }else{
-//                    
-//                    getAllFlightsFromTo();
-//                }
-//            };
 
             $scope.flights = [];
             $scope.depDate = new Date();
-            $scope.flyFrom = 'CPH';
+            $scope.flyFrom = '';
             $scope.flyTo = '';
             $scope.seats = 0;
-            
+
+            $scope.showSpinner = false;
             $scope.isError = false;
             $scope.errorMessage = "Sorry for the inconvenience";
-            
+
+            $scope.searchFlights = function () {
+
+                if ($scope.flyTo == '') {
+                    $scope.getAllFlightsFrom();
+                } else {
+                    $scope.getAllFlightsFromTo();
+                }
+            };
+
             $scope.getAllFlightsFrom = function () {
+                alert("in getAllFlightFrom")
+                $scope.showSpinner = true;
                 $scope.isError = false;
                 $scope.flights = [];//clear the flights
-                
+
                 //convert the data 
                 var year = $scope.depDate.getFullYear();
                 var month = $scope.depDate.getMonth();
                 var day = $scope.depDate.getDate();
-                
+
                 var rawDate = new Date(year, month, day, 1);
-                
+
                 //alert(year + "-" + month + "-" + day);
-                
+
                 var dateJSON = JSON.stringify(rawDate);//Convert into a JSON-string
                 var dateStr = JSON.parse(dateJSON);//Convert back into JavaScript
                 //var dateFromJson = new Date(dateStr);//JavaScript provides a Date Constructor that takes an ISO-8601 string
 
-                
+
                 InfoFactory.getAllFlightsFrom($scope.flyFrom, dateStr, $scope.seats)
-                
-                        .then(function (response) {
-                            $scope.flights = response.data;
-                        }, function (error) {
-                             $scope.isError = true;
-                             $scope.errorMessage = error.message;
-                        });
-                
-                
-            };
-            $scope.getAllFlightsFromTo = function () {
-                $scope.isError = false;//hide the error bar
-                $scope.flights = [];//clear the flights
-                
-                //convert the data 
-                var year = $scope.depDate.getFullYear();
-                var month = $scope.depDate.getMonth();
-                var day = $scope.depDate.getDate();
-                
-                var rawDate = new Date(year, month, day, 1);
-                
-                var dateJSON = JSON.stringify(rawDate);//Convert into a JSON-string
-                var dateStr = JSON.parse(dateJSON);//Convert back into JavaScript
-                //var dateFromJson = new Date(dateStr);//JavaScript provides a Date Constructor that takes an ISO-8601 string
 
-                
-                InfoFactory.getAllFlightsFromTo($scope.flyFrom, $scope.flyTo, dateStr, $scope.seats)
-                
                         .then(function (response) {
                             $scope.flights = response.data;
+                            $scope.showSpinner = false;
                         }, function (error) {
                             $scope.isError = true;
-                             $scope.errorMessage = error.message;
+                            $scope.errorMessage = error.message;
+                            $scope.showSpinner = false;
+                        });
+
+
+            };
+            $scope.getAllFlightsFromTo = function () {
+                $scope.showSpinner = true;
+                $scope.isError = false;//hide the error bar
+                $scope.flights = [];//clear the flights
+
+                //convert the data 
+                var year = $scope.depDate.getFullYear();
+                var month = $scope.depDate.getMonth();
+                var day = $scope.depDate.getDate();
+
+                var rawDate = new Date(year, month, day, 1);
+
+                var dateJSON = JSON.stringify(rawDate);//Convert into a JSON-string
+                var dateStr = JSON.parse(dateJSON);//Convert back into JavaScript
+                //var dateFromJson = new Date(dateStr);//JavaScript provides a Date Constructor that takes an ISO-8601 string
+
+
+                InfoFactory.getAllFlightsFromTo($scope.flyFrom, $scope.flyTo, dateStr, $scope.seats)
+
+                        .then(function (response) {
+                            $scope.flights = response.data;
+                            $scope.showSpinner = false;
+                        }, function (error) {
+                            $scope.isError = true;
+                            $scope.errorMessage = error.message;
+                            $scope.showSpinner = false;
                         });
             };
 
 
         });
-        
+
         
